4be9f9242010f98c727e90e444794a4ce5ede0df
Annie Cai <caishajia@gmail.com>
2022-07-01 20:31:26 -0400

    added about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  darkMode: boolean = false;
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
+  
+  constructor(private darkModeService: DarkModeService) { }
 
   ngOnInit(): void {
+    this.darkMode$.subscribe(data => this.darkMode = data);
   }
 
 }
